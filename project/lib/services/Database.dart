7da3063cbe3b504ec80296576726fb60dd1112a2
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2026-02-12 23:13:32 +0530

    Update Database.dart

--- project/lib/services/Database.dart
+++ project/lib/services/Database.dart
@@ -3,13 +3,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/DriverModel.dart';
 
-
 class DatabaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Future<void> saveDriverData(DriverModel driver) async {
     try {
-      await _db.collection('routes').doc(driver.uid).set(driver.toMap());
+      await _db.collection('driver').doc(driver.uid).set(driver.toMap());
     } catch (e) {
       print("Error saving driver: $e");
       rethrow;
