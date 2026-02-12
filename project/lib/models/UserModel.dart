f026e06e9823b4a44027911af4c769fc19b837a5
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2026-02-12 21:59:13 +0530

    Update UserModel.dart

--- project/lib/models/UserModel.dart
+++ project/lib/models/UserModel.dart
@@ -2,22 +2,11 @@ class MyUserModel {
   final String uid;
   final String? email;
   final String? displayName;
-  final String? photoUrl;
 
-  MyUserModel({
-    required this.uid,
-    this.email,
-    this.displayName,
-    this.photoUrl,
-  });
+  MyUserModel({required this.uid, this.email, this.displayName});
 
   // Factory constructor to create our model from a Firebase User object
   factory MyUserModel.fromFirebaseUser(dynamic user) {
-    return MyUserModel(
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoUrl: user.photoURL,
-    );
+    return MyUserModel(uid: user.uid, email: user.email);
   }
 }
